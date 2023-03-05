e345736fcd4a7a9a14bd85d3b8ade934e37d2899
Razvodov <razvodov.alexey@gmail.com>
2023-03-05 12:41:32 +0000

    hw4: staging fhv

---
+++ week4/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='view') }}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('fhv_staging','fhv_native') }}
+  where dispatching_base_num is not null 
+)
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num as text) as dispatching_base_num
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    cast(SR_Flag as text) as sr_flag,
+    cast(affiliated_base_number as text) as affiliated_base_number
+from tripdata
+where rn = 1
+
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=false) %}
+
+  limit 100
+
+{% endif %}
